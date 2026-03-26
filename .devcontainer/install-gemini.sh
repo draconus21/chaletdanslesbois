da89154e688cb0ee7135b53e5090892df494951a
Neeth Kunnath <neeth.xavier@gmail.com>
2026-03-26 05:33:35 +0000

    add gemini cli setup to devcontainer

---
+++ .devcontainer/install-gemini.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+
+# gemini
+npm install -g @google/gemini-cli
